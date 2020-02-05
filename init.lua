2480e09bee3c5505787c6a0a706e496a7b09786d
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-02-05 20:22:50 +0000

    ppwm is now used as a class

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 
-local pwms = require("pwms")
+local ppwm = require("ppwm")
 
 -- Used to determine the directional action of the led breathing affect.
 local direction = true
@@ -8,27 +8,42 @@ local direction = true
 -- chip limit. If the duty is currently lower than or equal to 0, then processed to increase it up
 -- to the chips duty limit. If the limit is hit, reverse the process until 0 again. incrementing in
 -- steps of 20.
-local function led_breathing_effect()
-  print("shifting duty: " .. pwms.get_duty())
+local function led_breathing_effect(pwms, shift)
+  return function()
+    print("shifting duty: " .. pwms:get_duty() .. " pin: " .. pwms.executing_pin)
 
-  if pwms.get_duty() <= 0 then
-    direction = true
-  elseif pwms.get_duty() >= pwms.duty_cycle_limit then
-    direction = false
-  end
+    if pwms:get_duty() <= 0 then
+      direction = true
+    elseif pwms:get_duty() >= pwms.duty_cycle_limit then
+      direction = false
+    end
 
-  if direction then
-    pwms.update_duty(pwms.get_duty() + 20)
-  else
-    pwms.update_duty(pwms.get_duty() - 20)
+    if direction then
+      pwms:update_duty(pwms:get_duty() + shift)
+    else
+      pwms:update_duty(pwms:get_duty() - shift)
+    end
+
+    end
   end
-end
 
 local function main()
-  pwms.configure(3, 500, pwms.duty_cycle_limit, true)
+  local p1 = ppwm:create(4)
+  p1:configure(1023)
+
+  local p2 = ppwm:create(3)
+  p2:configure(1023)
+
+  local p3 = ppwm:create(2)
+  p3:configure(1023)
 
   local pwm_timer = tmr.create()
-  pwm_timer:alarm(200, tmr.ALARM_AUTO, led_breathing_effect)
+  local pwm_timer2 = tmr.create()
+  local pwm_timer3 = tmr.create()
+
+  pwm_timer:alarm(50, tmr.ALARM_AUTO, led_breathing_effect(p1, 50))
+  pwm_timer2:alarm(100, tmr.ALARM_AUTO, led_breathing_effect(p2, 100))
+  pwm_timer3:alarm(200, tmr.ALARM_AUTO, led_breathing_effect(p3, 150))
 end
 
 main()
--- ppwm.lua
+++ ppwm.lua
@@ -1,4 +1,8 @@
-local M = {
+Ppwm = {}
+Ppwm.__index = Ppwm
+
+function Ppwm:create(pin, clock)
+  local this = {
   -- This is the wdth of the pulse based on the hardware. This can be adjusted to determine the
   -- output power. e.g maximum duty cycle would be classed as "on" while 0 could be classed as
   -- "off". Making sure to not provide too much power to the device and burning it out.
@@ -7,7 +11,7 @@ local M = {
   -- Resolution is determined by the hardware.
   --
   -- The current duty cycle limit for the current board.
-  duty_cycle_limit = 1024 - 1;
+  duty_cycle_limit = 1023;
 
   -- The frequency the PWM is executing within.
   clock_cycle = 1000;
@@ -20,7 +24,19 @@ local M = {
 
   -- If we are currently running or not.
   running = false;
-}
+  }
+
+  if pin ~= nil then
+    this.executing_pin = pin
+  end
+
+  if clock ~= nil then
+    this.clock_cycle = clock
+  end
+
+  setmetatable(this, Ppwm)
+  return this
+end
 
 -- configure sets up the pwm on a specified pin with a clock and duty cycle. If start is specified
 -- then the pwm will be setup and the duty will  be found to the pin.
@@ -29,49 +45,44 @@ local M = {
 -- clock      {number}:  The clock cycle of the specified pin.
 -- duty_cycle {number}:  The duty cycle of the specified pin, how much power.
 -- start      {boolean}: If configuration process should also start processing pwm data.
-local function configure(pin, clock, duty_cycle, start)
-    M.executing_pin = pin
-    M.clock_cycle = clock
-
-    if start then
-      M.PWM.setup(M.executing_pin, M.clock_cycle, M.duty_cycle_limit)
-      M.update_duty(duty_cycle)
-      M.running = true
-    end
+function Ppwm:configure(duty)
+    self.PWM.setup(self.executing_pin, self.clock_cycle, self.duty_cycle_limit)
+    self:update_duty(duty)
+    self.running = true
 end
 
 -- Stops the current proessing of the PWM data, marks internally that the PWM is not running and
 -- calls into the module to stop the PWM.
-local function stop()
-  if M.running then
-    M.running = false
-    M.PWM.stop()
+function Ppwm:stop()
+  if self.running then
+    self.running = false
+    self.PWM.stop()
   end
 end
 
 -- Completely disconnects and quits the PWM mode for the current executing pin. Ensures to
 -- stop/pause the processing of the data in pwm first before quiting the pwm mode for the current
 -- pin.
-local function close()
-  if M.running then
-    M.stop()
+function Ppwm:close()
+  if self.running then
+    self:stop()
   end
 
-  M.PWM.close()
+  self.PWM.close()
 end
-
 
 -- updateExecutingPin takes in a new pin that will processing the PWM and if currently executing,
 -- stops the current PWM (quiting), reconfigures and runs the pwm on the new pin and starts the
 -- processing again.
 --
 -- pin {number}: The chip pin that is being setup for pwm processing.
-local function update_executing_pin(pin)
-  if M.running then
-    M.close()
+function Ppwm:update_executing_pin(pin)
+  if self.running then
+    self:close()
   end
 
-  M.configure(pin, M.clock_cycle, M.get_duty(), true)
+  self.executing_pin = pin
+  self:configure()
 end
 
 -- Updates the duty for the current pin with the specified amount. Including the guarding of the
@@ -79,46 +90,35 @@ end
 -- problems but this will ensure the duty will not.
 --
 -- duty {number}: The duty cycle of the specified pin, how much power.
-local function update_duty(duty)
-  if duty > M.duty_cycle_limit then
-    duty = M.duty_cycle_limit
+function Ppwm:update_duty(duty)
+  if duty > self.duty_cycle_limit then
+    duty = self.duty_cycle_limit
   end
 
   if duty < 0 then
     duty = 0
   end
 
-  M.PWM.setduty(M.executing_pin, duty)
+  self.PWM.setduty(self.executing_pin, duty)
 end
 
 -- Increases the duty by the specified amount.
 --
 -- amount {number}: The amount to increase the duty.
-local function increase_duty(amount)
-  M.updateDuty(M.get_duty() + amount)
+function Ppwm:increase_duty(amount)
+  self.updateDuty(self:get_duty() + amount)
 end
 
 -- Decreases the duty by the specified amount.
 --
 -- amount {number}: The amount to decrease the duty.
-local function decrease_duty(amount)
-  M.updateDuty(M.get_duty() - amount)
+function Ppwm:decrease_duty(amount)
+  self:updateDuty(self:get_duty() - amount)
 end
 
 -- returns the current dity on the current pin.
-local function get_duty()
-  return M.PWM.getduty(M.executing_pin)
+function Ppwm:get_duty()
+  return self.PWM.getduty(self.executing_pin)
 end
 
-M.configure = configure
-M.stop = stop
-M.close = close
-
-M.update_executing_pin = update_executing_pin
-
-M.update_duty = update_duty
-M.increase_duty = increase_duty
-M.decrease_duty = decrease_duty
-M.get_duty = get_duty
-
-return M
+return Ppwm

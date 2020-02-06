78e86bfe93b0b16b22ff31228171eeb65cc22ea9
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-02-06 21:21:39 +0000

    implementation of a dht wrapper

---
+++ ddht.lua
@@ -0,0 +1,88 @@
+Ddht = {}
+Ddht.__index = Ddht
+
+function Ddht:create(pin)
+  local this = {
+    -- The pin number of the DHT sensor, cannot be 0, type of a number.
+    executing_pin = pin;
+    -- The reference to the global DHT object.
+    DHT = dht;
+    -- The last status that was determined when reading from the Ddht.
+    last_status = dht.OK,
+    last_response = nil,
+  }
+
+  setmetatable(this, Ddht)
+  return this
+end
+
+-- Reads all raw data coming from any dht sensor including the dh11.
+function Ddht:read_raw()
+  local status, temp, humi, temp_dec, humi_dec self.DHT.read(self.executing_pin)
+  return self:process_response(status, temp, humi, temp_dec, humi_dec)
+end
+
+-- Reads raw information from all non-dht11 sensors.
+function Ddht:read_raw_not_11()
+  local status, temp, humi, temp_dec, humi_dec = self.DHT.readxx(self.executing_pin)
+  return self:process_response(status, temp, humi, temp_dec, humi_dec)
+end
+
+-- Reads as if its a DHT11, taking the response status, temp and humdi with all its decimal values.
+-- Followed by post processing.
+function Ddht:read()
+  local status, temp, humi, temp_dec, humi_dec = self.DHT.read11(self.executing_pin)
+  return self:process_response(status, temp, humi, temp_dec, humi_dec)
+end
+
+-- process_response does some post read processing, ensuring tot track the status for internal
+-- referencing and formatting the repsonse into a easily handled object over multiple properties.
+function Ddht:process_response(status, temp, humi, temp_dec, humi_dec)
+ self.last_status = status;
+
+  self.last_response = {
+    temperature = temp,
+    temperature_decimal = temp_dec,
+    humidity = humi,
+    humidity_decimal = humi_dec
+  }
+
+  return self.last_response
+end
+
+-- returns true if the last sensor was ok.
+function Ddht:is_ok()
+  return self.last_status == self.DHT.OK
+end
+
+-- returns true if the last sensor was not ok.
+function Ddht:is_error()
+  return self.last_status ~= self.DHT.OK
+end
+
+-- returns true if the sensor is reporting the data otherwise false for no real value is being
+-- reported.
+function Ddht:is_reporting()
+  return self.last_response ~= nil and self.last_response.temperature ~= -999 and self.last_response.humidity ~= -999
+end
+
+-- retuns the last returned status from the chip or dht module, otherwise nil.
+function Ddht:get_status()
+  return self.last_status
+end
+
+-- returns a string based message for a given status. OK, ERROR_CHECKSUM, ERROR_TIMEOUT errors.
+-- Otherwise UNKNOWN.
+function Ddht:status_string()
+  if self.last_status == self.DHT.OK then
+    return "OK"
+  elseif self.last_status == self.DHT.ERROR_CHECKSUM then
+    return "CHECKSUM ERROR"
+  elseif self.last_status == self.DHT.ERROR_TIMEOUT then
+    return "TIMEOUT ERROR"
+  end
+
+  return "UNKNOWN"
+end
+
+return Ddht
--- init.lua
+++ init.lua
@@ -1,49 +1,11 @@
 
-local ppwm = require("ppwm")
-
--- Used to determine the directional action of the led breathing affect.
-local direction = true
-
--- Triggers a e.g breathing affect on a pwm pin by shifting the duty up and down the bounds of the
--- chip limit. If the duty is currently lower than or equal to 0, then processed to increase it up
--- to the chips duty limit. If the limit is hit, reverse the process until 0 again. incrementing in
--- steps of 20.
-local function led_breathing_effect(pwms, shift)
-  return function()
-    print("shifting duty: " .. pwms:get_duty() .. " pin: " .. pwms.executing_pin)
-
-    if pwms:get_duty() <= 0 then
-      direction = true
-    elseif pwms:get_duty() >= pwms.duty_cycle_limit then
-      direction = false
-    end
-
-    if direction then
-      pwms:update_duty(pwms:get_duty() + shift)
-    else
-      pwms:update_duty(pwms:get_duty() - shift)
-    end
-
-    end
-  end
+local ddht = require("ddht")
 
 local function main()
-  local p1 = ppwm:create(4)
-  p1:configure(1023)
+  local d1 = ddht:create(2)
+  local response = d1:read()
 
-  local p2 = ppwm:create(3)
-  p2:configure(1023)
-
-  local p3 = ppwm:create(2)
-  p3:configure(1023)
-
-  local pwm_timer = tmr.create()
-  local pwm_timer2 = tmr.create()
-  local pwm_timer3 = tmr.create()
-
-  pwm_timer:alarm(50, tmr.ALARM_AUTO, led_breathing_effect(p1, 50))
-  pwm_timer2:alarm(100, tmr.ALARM_AUTO, led_breathing_effect(p2, 100))
-  pwm_timer3:alarm(200, tmr.ALARM_AUTO, led_breathing_effect(p3, 150))
+  print("Status: " .. d1:status_string() .. " - DHT Temperature: " .. response.temperature .. " Humidity: " .. response.humidity)
 end
 
 main()

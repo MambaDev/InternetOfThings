12f63ea9622f36442d85b3fbc479e34a23f6a48e
Stephen Lineker-Miller <slinekermiller@gmail.com>
2020-03-11 18:07:28 +0000

    maint: updated init.template

--- weeks/init.template.lua
+++ weeks/init.template.lua
@@ -1,17 +1,24 @@
+local internet = require("lib_internet");
+local time = require("lib_time");
 
-local internet = require("internet")
+-- lib used
+-- rtctime, sntp, cron, http, sjson
 
 local function on_internet_connected()
-  print("internet connected")
+  print("Internet connected");
+  print("Syncronizating with internet clock")
+  time.clock_syncronization()
 end
 
 local function on_internet_disconnected()
-  print("internet disconnected")
+  print("Internet disconnected")
 end
 
 local function main()
-  internet.configure("Stephen", "password")
-  internet.connect(on_internet_connected, on_internet_disconnected)
+  internet.configure_station("The Promise Lan", "DangerZone2018", nil)
+  internet.connect_station(on_internet_connected, nil, on_internet_disconnected)
 end
 
+
 main()
+
